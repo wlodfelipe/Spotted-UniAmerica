b4fd2bd384f0c356f606174876fa12a7e0c8b85e
Felipe Luiz Wlodkowski <57642609+wlodfelipe@users.noreply.github.com>
2024-08-14 11:52:03 -0300

    First commit

---
+++ spotteduniamerica/src/main/java/com/spotteduniamerica/spotteduniamerica/SpotteduniamericaApplication.java
@@ -0,0 +1,13 @@
+package com.spotteduniamerica.spotteduniamerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotteduniamericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotteduniamericaApplication.class, args);
+	}
+
+}
---
+++ spotteduniamerica/src/test/java/com/spotteduniamerica/spotteduniamerica/SpotteduniamericaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotteduniamerica.spotteduniamerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotteduniamericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
